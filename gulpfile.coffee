c01431ed6645bfba554da5cf232f6e4ddb3f2bd8
Midiparse <david.szakallas@gmail.com>
2015-09-02 22:31:34 +0200

    Implement some controls

--- gulpfile.coffee
+++ gulpfile.coffee
@@ -1,11 +1,10 @@
 gulp          = require 'gulp'
-watch         = require 'gulp-watch'
-plumber       = require 'gulp-plumber' # Fix pipes on errors.
 lazypipe      = require 'lazypipe' # We need this to make reusable pipes. How sad is that?
 autoprefixer  = require 'gulp-autoprefixer'
 sass          = require 'gulp-sass'
 sourcemaps    = require 'gulp-sourcemaps'
 coffee        = require 'gulp-coffee'
+livereload    = require 'gulp-livereload'
 
 stylePipe = lazypipe()
   .pipe sourcemaps.init
@@ -13,53 +12,40 @@ stylePipe = lazypipe()
   .pipe autoprefixer, browsers: ['last 2 versions']
   .pipe sourcemaps.write
 
+
 gulp.task 'lib', ->
   gulp.src 'src/server/**/*.coffee'
     .pipe coffee()
     .pipe gulp.dest 'lib'
 
-gulp.task 'watch-index', ->
-  gulp.src 'src/client/index.html'
-    .pipe watch 'src/client/index.{htm,html}' #How shitty is that you can't watch paths without wildcards?
-    .pipe plumber()
-    .pipe gulp.dest 'public'
-
-gulp.task 'watch-parts', ->
-  gulp.src 'src/client/parts/*.html'
-    .pipe watch 'src/client/parts/*.html'
-    .pipe plumber()
-    .pipe gulp.dest 'public/parts'
-
-gulp.task 'watch-styles', ->
-  gulp.src 'src/client/styles/*.sass'
-    .pipe watch 'src/client/styles/*.sass'
-    .pipe stylePipe()
-    .pipe gulp.dest 'public/css'
-
-gulp.task 'watch-scripts', ->
-  gulp.src 'src/client/scripts/*.js'
-    .pipe watch 'src/client/scripts/*.js'
-    .pipe plumber()
-    .pipe gulp.dest 'public/js'
-
 gulp.task 'index', ->
   gulp.src 'src/client/index.html'
     .pipe gulp.dest 'public'
+    .pipe livereload()
 
 gulp.task 'parts', ->
   gulp.src 'src/client/parts/*.html'
     .pipe gulp.dest 'public/parts'
+    .pipe livereload()
 
 gulp.task 'styles', ->
   gulp.src 'src/client/styles/*.sass'
     .pipe stylePipe()
     .pipe gulp.dest 'public/css'
+    .pipe livereload()
 
 gulp.task 'scripts', ->
   gulp.src 'src/client/scripts/*.js'
     .pipe gulp.dest 'public/js'
+    .pipe livereload()
 
+gulp.task 'watch', ->
+  livereload.listen()
+  gulp.watch 'src/client/index.{htm,html}', ['index']
+  gulp.watch 'src/client/styles/*.sass', ['styles']
+  gulp.watch 'src/client/parts/*.html', ['parts']
+  gulp.watch 'src/client/scripts/*.js', ['scripts']
 
 gulp.task 'build', [ 'index', 'styles', 'lib', 'parts', 'scripts' ]
-gulp.task 'watch', [ 'watch-index', 'watch-styles', 'watch-parts', 'watch-scripts' ]
+
 gulp.task 'default', [ 'build' ]
--- src/client/scripts/app.js
+++ src/client/scripts/app.js
@@ -1,6 +1,200 @@
 ;(function(){
   'use strict';
 
-  var app = angular.module('Pasty', [ 'ui.ace' ]);
+  angular.module('Pasty', [ 'ui.ace' ])
+    .run(function() {
+
+      //3rd party stuff
+
+      $(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+      });
+
+    })
+    .controller('Main', [ '$scope', '$location', 'pasty', '$q', function($scope, $location, pasty, $q){
+
+      var location = {
+        id: function(id) {
+          if (typeof(id) === 'undefined')
+            return $location.path().substr(1);
+          else
+            $location.path("/" + id);
+        },
+        password: function(pass) {
+          if (typeof(pass) === 'undefined')
+            return $location.hash();
+          else
+            $location.hash(pass);            
+        }
+      };
+
+      var generatePassword = function() {
+          return sjcl.codec.base64url.fromBits(sjcl.random.randomWords(4));
+      };
+
+      var messaging = {
+
+        wrap: function(content) {
+          var randoms = sjcl.random.randomWords(3);
+
+          var params = {
+            cipher: 'aes',
+            ks: 256,
+            mode: 'gcm',
+            ts: 64,
+            iter: 1000,
+            iv: randoms[0]
+          };
+          
+          var plaintext = JSON.stringify(content);
+
+          var cyphertext = sjcl.encrypt(location.password(), plaintext, params, params);
+
+          var hmac = sjcl.codec.hex.fromBits(new sjcl.misc.hmac(
+            params.key, 
+            sjcl.hash.sha256
+          ).encrypt(plaintext));
+
+          return {
+            hmac: hmac,
+            content: cyphertext
+          };
+
+        },
+
+        unwrap: function(message) {
+
+          var params = {};
+
+          var plaintext = sjcl.decrypt(location.password(), message.content, params, params);
+
+          var hmac = sjcl.codec.hex.fromBits(new sjcl.misc.hmac(
+            params.key, 
+            sjcl.hash.sha256
+          ).encrypt(plaintext));           
+
+          if(hmac === message.hmac)
+            return JSON.parse(plaintext);
+          else 
+            throw new Error("Couldn't verify message contents.");
+
+        }
+
+      };
+
+      var watchForChanges = function(id, socket, editor) {
+
+        var d = $q.defer();
+
+        socket.on('change', function(data) {
+          console.log("Pasty " + id + " changed by someone");
+          try {
+            editor.setValue(messaging.unwrap(data));
+          } catch (e) {
+            if(e instanceof TypeError) 
+              console.log("Failed to decrypt message.");
+            else
+              console.log(e.message);
+          }
+        });
+
+        socket.on('disconnect', function() {
+          //@TODO insert alert popup
+          console.log('Pasty ' + id + ' disconnected');
+          // only resolve on disconnect
+          d.resolve();
+        });
+
+        editor.on('change', function(delta) {
+          //no delta yet, just send everything
+          
+          if (editor.curOp && editor.curOp.command.name) {
+            socket.emit('change', messaging.wrap(editor.getValue()));
+          }
+
+        });
+
+        var idWatch = $scope.$watch(function(){ return $location.path(); }, function(newVal, oldVal){
+          if(oldVal != newVal) {
+            console.log("Location changed. Disconnecting from Pasty " + oldVal);
+            pasty.disconnect(socket);
+            idWatch(); // kill the watches, no need for it anymore
+            //Not sure if this needed here, but better resolve twice than none.
+            d.resolve();
+          }
+        });
+
+
+
+        console.log("Subscribed to changes on Pasty " + id);
+
+        return d.promise;
+      };
+
+
+      var main = function(id, editor) {
+
+        var d = $q.defer();
+
+        if(id) {
+          pasty.connect(id).then(
+            function(socket){
+              d.resolve(watchForChanges(id, socket, editor));
+            },
+            function(e){
+              console.log('Could not connect to Pasty ' + id);
+
+              if(e === "not found") {
+                console.log("Pasty " + id + " not found. Creating it...")
+                pasty.create(id).then(
+                  function(id) {
+                    console.log("Pasty " + id + " created");
+                    location.id(id);
+                    location.password(generatePassword());
+                    d.resolve();
+                  },
+                  function(e) {
+                    console.log("Server refused creating Pasty " + id)
+                    location.id("");
+                    location.password("");
+                    d.resolve();
+                  });
+              } else {
+                location.id("");
+                location.password("");
+                d.resolve();
+              }
+            }
+          );
+        } else {
+          console.log("Empty URL. Creating new Pasty...");
+          pasty.create().then(function(id) {
+            console.log("Pasty " + id + " created");
+            location.id(id);
+            location.password(generatePassword());
+            d.resolve();
+          });
+        }
+        return d.promise;
+      };
+
+
+      $scope.aceOption = {
+        onLoad: function (ace) {
+          ace.$blockScrolling = Infinity
+
+          //Non-blocking loop
+
+          //changes from true to false to true to false.. etc
+          var tick = true;
+
+          $scope.$watch(
+            function(){ return tick; }, 
+            function(){ main(location.id(), ace).then( function() { tick = !tick; } ); }
+          );
+        }
+      };
+
+    }]);
 
 })();
